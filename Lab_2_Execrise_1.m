a818d1e465881422aaead6dce8c835f82e6e0040
Guozhen Ding <dingguoz@WIN.ecf.utoronto.ca>
2019-09-30 16:55:42 -0400

    add files

---
+++ Lab_2_Execrise_1.m
@@ -0,0 +1,13 @@
+clear all ;
+x1 = [0:5:20] ;
+y1 = [200 152 118 93 74];
+%plot ( x1, y1);
+R = 5; 
+xishu = polyfit ( x1 , y1 , R )
+x2 = [0:0.1:20]
+y2 = [zeros(1,201)]
+for i = 1:R+1
+    temp = xishu(i).*x2.^(R-i+1);
+    y2 = y2 + temp;
+end
+plot (x2, y2)
